14ebfb6d81d90b0395c671a08679f4e23bf6226c
Pedro Marques <pdro.marqs2015@hotmail.com>
2025-11-08 00:09:12 -0300

    Criando arquivo local de banco que armazena os usuarios cadastrados (passando de stateful para stateless)

--- src/database.js
+++ src/database.js
@@ -1,6 +1,26 @@
+import fs from 'node:fs/promises';
+
+// Instanciando uma classe interna do Javascript, URL, para capturar a url do arquivo db.json e ter o path dele
+const databasePath = new URL('../db.json', import.meta.url);
+
 export class DataBase {
     // Hashtag é o jeito node de deixar uma propriedade privada. Agora novas instancias da classe nao podem acessar essa propriedade (equivalente a private)
     #database = {};
+
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+        .then(data => {
+            this.#database = JSON.parse(data);
+        })
+        .catch(() => {
+            this.#persist();
+        });
+    }
+
+    // Metodo que escreve no arquivo de banco local
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    }
 
     select(table) {
         const data = this.#database[table] ?? [];
@@ -14,6 +34,8 @@ export class DataBase {
         } else {
             this.#database[table] = [data];
         }
+
+        this.#persist();
 
         return data;
     }
