413984dbf8e5cc3bed03feefc13f699872743382
Pedro Marques <pdro.marqs2015@hotmail.com>
2025-11-10 22:57:29 -0300

    Criando Regex para encontrar rota com Route Params de :id (Rota de delete user)

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js";
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (request, response) => {
             const users = database.select('users');
             const usersStringified = JSON.stringify(users);
@@ -16,7 +17,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (request, response) => {
             if(request.body === null) return response.writeHead(400).end('Body não possui valor valido (name, email)');
 
@@ -32,6 +33,13 @@ export const routes = [
             database.insert('users', user);
 
             return response.writeHead(201).end();
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (request, response) => {
+            return response.end();
         }
     }
 ]
